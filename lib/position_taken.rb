e94e69d1a92ca58b6405c104279fd2661100f3c1
summer trainer <summertrainer@yahoo.com>
2021-06-26 21:37:49 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,8 @@
 # code your #position_taken? method here!
+def position_taken?(board, position)
+  if board[position] == " " || ""
+    taken = false
+  else board[position] == "X" || "O"
+    taken = true
+  end
+end
