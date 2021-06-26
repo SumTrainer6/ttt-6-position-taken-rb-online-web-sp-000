774d200d51158b06da9d773bd4df94ce98d116b9
summer trainer <summertrainer@yahoo.com>
2021-06-26 22:19:00 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -4,7 +4,7 @@ def position_taken?(board, position)
   # binding.pry
   if board[position] == " " || board[position] == "" || board[position] == nil 
     taken = false
-  elsif board[position] == "X" || board[position] == "O"
+  else board[position] == "X" || board[position] == "O"
     taken = true
   end
 end
