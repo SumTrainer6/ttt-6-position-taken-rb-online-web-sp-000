6960023186091680ccf6ab05c654e354861eb2e6
summer trainer <summertrainer@yahoo.com>
2021-06-26 22:09:28 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,7 +1,7 @@
 require "pry"
 # code your #position_taken? method here!
 def position_taken?(board, position)
-  binding.pry
+  # binding.pry
   if board[position] == " " || board[position] == ""
     taken = false
   else board[position] == "X" || board[position] == "O"
